0e423a393e08b4f782f1fd61947be58b6609a256
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-29 14:24:55 +0530

    submitting code-Tue Aug 29 2023 14:24:54 GMT+0530 (India Standard Time)

--- src/components/Loadmore.js
+++ src/components/Loadmore.js
@@ -1,37 +1,36 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
+const LoadMore = ({ items }) => {
+  const [displayedItems, setDisplayedItems] = useState([]);
+  const [page, setPage] = useState(0);
 
-const Loadmore=({items})=>{
-    const [displayItems,setDisplayItems]=useState([]);
-    const [page,setPage]=useState(0);
+  const loadMore = () => {
+    const nextPage = page + 1;
+    const startIndex = (nextPage - 1) * 10;
+    const endIndex = startIndex + 10;
 
+    // Load next 10 items asynchronously
+    setTimeout(() => {
+      setDisplayedItems([
+        ...displayedItems,
+        ...items.slice(startIndex, endIndex)
+      ]);
+      setPage(nextPage);
+    }, 500);
+  };
 
+  return (
+    <div>
+      <ul>
+        {displayedItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      {displayedItems.length < items.length && (
+        <button onClick={loadMore}>Load More</button>
+      )}
+    </div>
+  );
+};
 
-    const loadMore=()=>{
-        const nextPage=page+1;
-        const startIndex=(nextPage-1)*10;
-        const endIndex=startIndex+10;
-
-        setTimeout(()=>{
-            setDisplayItems([...displayItems,items.slice(startIndex,endIndex)]);
-            setPage(nextPage);
-        },500);
-    };
-
-    return (
-        <div>
-            <ul>
-                {
-                    displayItems.map((item,index)=>{
-                        <li key={index}>{item}</li>
-                    })
-                }
-            </ul>
-            {
-                displayItems.length<items.length && (<button onClick={loadMore}>Load More</button>)
-            }
-        </div>
-    )
-}
-
-export default Loadmore;
+export default LoadMore;
